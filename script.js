e51b51cea6b1a8eb0d8d31482a070712bfd5c173
fierrogt <108372808+fierrogt@users.noreply.github.com>
2022-06-29 11:45:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-$('#main_clock').fitText(1.3);
-$('#clock1').fitText(1.3);
-$('#clock2').fitText(1.3);
-$('#clock3').fitText(1.3);
+var london = moment.tz("Europe/London").format('MMMM Do YYYY, h:mm:ss a');
+var sydney = moment.tz("Australia/Sydney").format('MMMM Do YYYY, h:mm:ss a');
+var beijing = moment.tz("China/Beijing").format('MMMM Do YYYY, h:mm:ss a');
+var tokyo = moment.tz("Asia/Tokyo").format('MMMM Do YYYY, h:mm:ss a');
+var la = moment.tz("America/Los_Angeles").format('MMMM Do YYYY, h:mm:ss a');
+
+$("#london").text(london);
+$("#sydney").text(sydney);
+$("#beijing").text(beijing);
+$("#tokyo").text(tokyo);
+$("#la").text(la);
+
 function update() {
-  $('#main_clock').html(moment().format('H:mm a'));
-  $('#clock1').html(moment.tz('America/Los_Angeles').format('H:mm a'));
-  $('#clock2').html(moment.tz('Europe/London').format('H:mm a'));
+    $('#la').html(moment().format('h:mm a'));
+    $('#london').html(moment().format('h:mm a'));
+    $('#tokyo').html(moment().format('h:mm a'));
+    $('#beijing').html(moment().format('h:mm a'));
+    $('#sydney').html(moment().format('h:mm a'));
 }
+
+
 
 setInterval(update, 1000);
