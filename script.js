0a9a0f3fde14939c4772e680e79e0ac258943637
fierrogt <108372808+fierrogt@users.noreply.github.com>
2022-06-29 11:11:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $('#clock').fitText(1.3);
 
 function update() {
-  $('#clock').html(moment().format('D. MMMM YYYY H:mm:ss'));
+  $('#clock').html(moment().format('H:mm a'));
 }
 
 setInterval(update, 1000);
