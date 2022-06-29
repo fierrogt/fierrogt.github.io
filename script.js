3606d329dd5ecb5bf9eadb460d50bc56f32e9b6b
fierrogt <108372808+fierrogt@users.noreply.github.com>
2022-06-29 11:26:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-$('#clock').fitText(1.3);
-
+$('#main_clock').fitText(1.3);
+$('#clock1').fitText(1.3);
+$('#clock2').fitText(1.3);
+$('#clock3').fitText(1.3);
 function update() {
-  $('#clock').html(moment().format('H:mm a'));
+  $('#main_clock').html(moment().format('H:mm a'));
+  $('#clock1').html(moment().tz('America/Los_Angeles').format('H:mm a'));
+  $('#clock2').html(moment().tz('Europe/London').format('H:mm a'));
 }
 
 setInterval(update, 1000);
